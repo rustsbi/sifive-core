fd5b2def58becab3fcbebeca93abf33d9933b435
luojia65 <me@luojia.cc>
2021-12-02 16:05:27 +0800

    Rearrange documents; remove PAUSE instruction

--- src/asm.rs
+++ src/asm.rs
@@ -1,182 +1,181 @@
 //! Assembly instructions
+//!
+//! # Absence of PAUSE instruction
+//!
+//! SiFive cores may have PAUSE instruction implemented, which is also defined in RISC-V extension
+//! Zhintpause. This extension is adapted to [`core::hint::spin_loop()`] function in Rust core crate,
+//! and thus not implemented separately in platform specific assembly instruction module.
+//!
+//! [`core::hint::spin_loop()`]: https://doc.rust-lang.org/stable/core/hint/fn.spin_loop.html
 
 const RS1_SHIFT: usize = 15;
 const XREG_A0: usize = 10;
 
 /// CEASE, core halt instruction
-/// 
+///
 /// This function will never return and will immediately cease the current hart.
-/// 
-/// # Privilege mode perimissions
-/// 
+///
+/// # Unsafety
+///
+/// Calling this function is unsafe, because the resource this function uses
+/// is not freed (i.e. `fn drop` in Drop trait is not called) after current hart ceased.
+/// Caller must ensure that all resources are freed before invoking CEASE instruction.
+///
+/// # Privilege mode permissions
+///
 /// This is a privileged instruction and it's only available in M-mode.
-/// 
+///
+/// # Example
+///
+/// ```no_run
+/// #[panic_handler]
+/// fn machine_panic(info: &PanicInfo) -> ! {
+///     print_stack_trace_and_information(info);
+///     unsafe { sifive_core::asm::cease() }
+/// }
+/// ```
+///
 /// # Hardware implmenetaion
 ///
 /// After retiring CEASE, hart will not retire another instruction until reset.
-/// 
-/// Instigates power-down sequence, which will eventually raise the `cease_from_tile_N` signal 
+///
+/// Instigates power-down sequence, which will eventually raise the `cease_from_tile_N` signal
 /// to the outside of the Core Complex, indicating that it is safe to power down.
-/// 
+///
 /// CEASE has no effect on System Bus Access.
-/// 
+///
 /// Debug `haltreq` will not work after a CEASE instruction has retired.
 #[inline]
 pub unsafe fn cease() -> ! {
     asm!(".word 0x30500073", options(noreturn))
 }
 
-/// PAUSE, spin-wait loop idle instruction
-/// 
-/// This instruction may be used for more efficient idling in spin-wait loops.
-/// 
-/// PAUSE is a FENCE instruction with predecessor set W and null successor set. 
-/// Therefore, PAUSE is a HINT instruction that executes as a no-op on all RISC-V implementations.
-/// 
-/// # Hardware implmenetaion
-/// 
-/// This instruction causes a stall of up to 32 cycles or until a cache eviction occurs, whichever comes first.
-#[inline]
-pub fn pause() {
-    unsafe { 
-        asm!(".word 0x0100000F")
-    }
-}
-
 /// CFLUSH.D.L1 x0, L1 data cache full-cache flush instruction
-/// 
+///
 /// This instruction writes back and invalidates all lines in the L1 data cache.
-/// 
+///
 /// # Hardware implmenetaion
-/// 
+///
 /// Implemented as state machine in L1 data cache, for cores with data caches.
-/// 
-/// # Privilege mode perimissions
-/// 
+///
+/// # Privilege mode permissions
+///
 /// Only available in M-mode.
-/// 
+///
 /// # Platform support
-/// 
+///
 /// CFLUSH.D.L1 full-cache flush instruction is supported by all SiFive® Performance™ cores,
 /// all SiFive® Intelligence™ cores, and SiFive® Essential™ U7, U5, S7 and E7 cores.
-/// 
+///
 // supported by U74, U74-MC, U54, U54-MC, S76, S76-MC, E76 and E76-MC
 // *not* supported by S54, S51, S21, E34, E31, E24, E21 and E20 cores
-// 
+//
 /// If this instruction is not supported by current platform, an illegal-instruction exception is raised.
 #[inline]
 pub fn cflush_d_l1_all() {
-    unsafe { 
-        asm!(".word 0xFC000073")
-    }
+    unsafe { asm!(".word 0xFC000073") }
 }
 
 /// CFLUSH.D.L1 rs1, L1 data cache flush virtual address instruction
-/// 
+///
 /// This instruction writes back and invalidates the L1 data cache line containing
 /// the virtual address in integer register rs1.
-/// 
+///
 /// # Hardware implmenetaion
-/// 
+///
 /// Implemented as state machine in L1 data cache, for cores with data caches.
-/// 
-/// # Privilege mode perimissions
-/// 
+///
+/// # Privilege mode permissions
+///
 /// Only available in M-mode.
-/// 
+///
 /// # Exceptions
-/// 
+///
 /// If the effective privilege mode does not have write permissions to the address in rs1, then
 /// a store access or store page-fault exception is raised.
-/// 
+///
 /// If the address in rs1 is in an uncacheable region with write permissions, the instruction has
 /// no effect but raises no exceptions.
-/// 
+///
 /// Note that if the PMP scheme write-protects only part of a cache line, then using a value for
 /// rs1 in the write-protected region will cause an exception, whereas using a value for rs1 in
 /// the write-permitted region will write back the entire cache line.
-/// 
+///
 /// # Platform support
-/// 
-/// CFLUSH.D.L1 with rs1 ≠ x0 is supported by SiFive® Performance™ P550, P550-MC, 
+///
+/// CFLUSH.D.L1 with rs1 ≠ x0 is supported by SiFive® Performance™ P550, P550-MC,
 /// SiFive® Essential™ S76, S76-MC, E76 and E76-MC cores.
-/// 
-// CFLUSH.D.L1 with rs1 ≠ x0 is *not* supported on SiFive® Performance™ P270, P270-MC, 
+///
+// CFLUSH.D.L1 with rs1 ≠ x0 is *not* supported on SiFive® Performance™ P270, P270-MC,
 // SiFive® Intelligence™ X280, X280-MC,
 // SiFive® Essential™ U74, U74-MC, U54, U54-MC, S54, S51, S21, E34, E31, E24, E21 and E20 cores.
-// 
+//
 /// If this instruction is not supported by current platform, an illegal-instruction exception is raised.
 #[inline]
 pub fn cflush_d_l1_va(va: usize) {
-    unsafe { 
-        asm!(".word {}", const 0xFC000073 + (XREG_A0 << RS1_SHIFT), in("a0") va)
-    }
+    unsafe { asm!(".word {}", const 0xFC000073 + (XREG_A0 << RS1_SHIFT), in("a0") va) }
 }
 
 /// CDISCARD.D.L1 x0, L1 data cache full-cache invalidate instruction
-/// 
+///
 /// This instruction invalidates, but does not write back, all lines in the L1 data cache.
 /// Dirty data within the cache is lost.
-/// 
+///
 /// # Hardware implmenetaion
-/// 
+///
 /// Implemented as state machine in L1 data cache, for cores with data caches.
-/// 
-/// # Privilege mode perimissions
-/// 
+///
+/// # Privilege mode permissions
+///
 /// Only available in M-mode.
-/// 
+///
 /// # Platform support
-/// 
+///
 /// CDISCARD.D.L1 full-cache invalidate instruction is supported by all SiFive® Performance™ cores,
 /// all SiFive® Intelligence™ cores, and SiFive® Essential™ U7, U5, S7 and E7 cores.
-/// 
+///
 // supported by U74, U74-MC, U54, U54-MC, S76, S76-MC, E76 and E76-MC
 // *not* supported by S54, S51, S21, E34, E31, E24, E21 and E20 cores
-// 
+//
 /// If this instruction is not supported by current platform, an illegal-instruction exception is raised.
 #[inline]
 pub fn cdiscard_d_l1_all() {
-    unsafe { 
-        asm!(".word 0xFC200073")
-    }
+    unsafe { asm!(".word 0xFC200073") }
 }
 
 /// CDISCARD.D.L1 rs1, L1 data cache invalidate virtual address instruction
-/// 
-/// This instruction invalidates, but does not write back, the L1 data cache line containing 
-/// the virtual address in integer parameter `va`. 
+///
+/// This instruction invalidates, but does not write back, the L1 data cache line containing
+/// the virtual address in integer parameter `va`.
 /// Dirty data within the cache line is lost.
-/// 
+///
 /// # Hardware implmenetaion
-/// 
+///
 /// Implemented as state machine in L1 data cache, for cores with data caches.
-/// 
-/// # Privilege mode perimissions
-/// 
+///
+/// # Privilege mode permissions
+///
 /// Only available in M-mode.
-/// 
+///
 /// # Exceptions
-/// 
+///
 /// If the effective privilege mode does not have write permissions to the address in rs1, then
 /// a store access or store page-fault exception is raised.
-/// 
+///
 /// If the address in rs1 is in an uncacheable region with write permissions, the instruction has
 /// no effect but raises no exceptions.
-/// 
+///
 /// Note that if the PMP scheme write-protects only part of a cache line, then using a value for
 /// rs1 in the write-protected region will cause an exception, whereas using a value for rs1 in
 /// the write-permitted region will invalidate and discard the entire cache line.
-/// 
+///
 /// # Platform support
-/// 
+///
 /// CDISCARD.D.L1 virtual address cache invalidate instruction is supported by all SiFive® Performance™ cores,
 /// all SiFive® Intelligence™ cores, and SiFive® Essential™ U7, U5, S7 and E7 cores.
 ///
 /// If this instruction is not supported by current platform, an illegal-instruction exception is raised.
 #[inline]
 pub fn cdiscard_d_l1_va(va: usize) {
-    unsafe { 
-        asm!(".word {}", const 0xFC200073 + (XREG_A0 << RS1_SHIFT), in("a0") va)
-    }
+    unsafe { asm!(".word {}", const 0xFC200073 + (XREG_A0 << RS1_SHIFT), in("a0") va) }
 }
--- src/register.rs
+++ src/register.rs
@@ -1,14 +1,14 @@
 //! Platform specific SiFive CSRs
 
 /// Branch prediction mode register
-/// 
+///
 /// This SiFive custom extension adds an M-mode CSR to control the current branch prediction mode, bpm at CSR 0x7C0.
-/// 
-/// Depending on platform, the Core Complex’s branch prediction system may include a Return Address Stack (RAS), 
-/// a Branch Target Buffer (BTB), and a Branch History Table (BHT). 
-/// 
-/// While branch predictors are essential to achieve high performance in pipelined processors, they can also cause 
-/// undesirable timing variability for hard real-time systems. The bpm register provides a means to customize 
+///
+/// Depending on platform, the Core Complex’s branch prediction system may include a Return Address Stack (RAS),
+/// a Branch Target Buffer (BTB), and a Branch History Table (BHT).
+///
+/// While branch predictors are essential to achieve high performance in pipelined processors, they can also cause
+/// undesirable timing variability for hard real-time systems. The bpm register provides a means to customize
 /// the branch predictor behavior to trade average performance for a more predictable execution time
 pub mod mbpm {
     use bit_field::BitField;
@@ -16,13 +16,13 @@ pub mod mbpm {
     #[derive(Clone, Copy, Debug)]
     #[repr(transparent)]
     pub struct Mbpm {
-        bits: usize
+        bits: usize,
     }
     impl Mbpm {
-        /// Branch-Direction Prediction. Determines the value returned by the BHT component of the branch prediction system. 
-        /// 
-        /// A zero value indicates dynamic direction prediction, and a non-zero value indicates static-taken direction prediction. 
-        /// 
+        /// Branch-Direction Prediction. Determines the value returned by the BHT component of the branch prediction system.
+        ///
+        /// A zero value indicates dynamic direction prediction, and a non-zero value indicates static-taken direction prediction.
+        ///
         /// The BTB is cleared on any write to bdp, and the RAS is unaffected by writes to bdp.
         #[inline]
         pub fn bdp(&self) -> bool {
@@ -36,7 +36,7 @@ pub mod mbpm {
         unsafe { asm!("csrr {}, 0x7C0", out(reg) bits) };
         Mbpm { bits }
     }
-    /// Set mode to dynamic direction prediction. 
+    /// Set mode to dynamic direction prediction.
     #[inline]
     pub unsafe fn clear_bdp() {
         asm!("csrrci 0x7C0, 0")
@@ -49,21 +49,21 @@ pub mod mbpm {
 }
 
 /// Feature disable register
-/// 
+///
 /// The SiFive custom M-mode Feature Disable CSR is provided to enable or disable certain
 /// microarchitectural features.
-/// 
+///
 /// A feature is fully enabled when the associated bit is zero. If a particular core does not support
 /// the disabling of a feature, the corresponding bit is hardwired to zero.
-/// 
+///
 /// On reset, all implemented bits are set to 1, disabling all features. The bootloader is responsible
-/// for turning on all required features and can simply write zero to turn on the maximal set of features. 
-/// SiFive’s Freedom Metal bootloader handles turning on these features; when using a custom bootloader, 
+/// for turning on all required features and can simply write zero to turn on the maximal set of features.
+/// SiFive’s Freedom Metal bootloader handles turning on these features; when using a custom bootloader,
 /// clearing the Feature Disable CSR must be implemented.
-/// 
-/// Note that arbitrary toggling of the Feature Disable CSR bits is neither recommended nor supported; 
-/// they are only intended to be set from 1 to 0. A particular Feature Disable CSR bit is only to be 
-/// used in a very limited number of situations, as detailed in the Example Usage entry in tables 
+///
+/// Note that arbitrary toggling of the Feature Disable CSR bits is neither recommended nor supported;
+/// they are only intended to be set from 1 to 0. A particular Feature Disable CSR bit is only to be
+/// used in a very limited number of situations, as detailed in the Example Usage entry in tables
 /// on documentation of each core.
 pub mod mfeature {
     use crate::feature::Mask;
